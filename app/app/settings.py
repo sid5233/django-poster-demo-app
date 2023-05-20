f02490a2c47f096f61c3ce36b6bf119e55a13ab8
Sudarshan Bhamare <sudarshan.bhamare@gmail.com>
2023-05-21 00:06:32 +0530

    added allowed host

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '34.236.158.134'
+]
 ALLOWED_HOSTS.extend(
     filter(
         None,
